ad1494453de0c7aa70e3e90361c4423ce105f6a3
Paulo Abdanur <ph_abdanur@yahoo.com.br>
2023-02-10 15:57:52 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import * as C from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
 
-function App() {
+const App = () => {
+  const char = useCharacter('Paulin');
+  const char2 = useCharacter('Thalita');
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.code) {
+      case 'KeyW':
+        char.moveUp();
+        break;
+      case 'ArrowUp':
+        char2.moveUp();
+        break;
+
+      case 'KeyS':
+        char.moveDown();
+        break;
+      case 'ArrowDown':
+        char2.moveDown();
+        break;
+
+      case 'KeyA':
+        char.moveLeft();
+        break;
+      case 'ArrowLeft':
+        char2.moveLeft();
+        break;
+
+      case 'KeyD':
+        char.moveRight();
+        break;
+      case 'ArrowRight':
+        char2.moveRight();
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Character
+          x={char2.x}
+          y={char2.y}
+          side={char2.side}
+          name={char2.name}
+        />
+      </C.Map>
+    </C.Container>
   );
-}
+};
 
 export default App;
---
+++ src/types/CharacterSides.ts
@@ -0,0 +1,1 @@
+export type CharacterSides = 'up' | 'down' | 'left' | 'right';
